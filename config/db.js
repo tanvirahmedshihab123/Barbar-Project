13b3b1b83349e0ce3d5e735c1af235a759a90fb7
tanvirahmedshihab123 <shihabtanvir03@gmail.com>
2024-08-12 23:32:20 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://jamilakhatun000000:Shihab123045@cluster0.zdouq8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("Database Connected"));
+    await mongoose.connect('mongodb+srv://sabatanvir2019:Shihab123045@cluster0.lukdc7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("Database Connected"));
 }
 
 
